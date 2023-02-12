607ba0b9636678aedf6887c07b2f74a041c815b8
Алексей Лисичников <alexochanel@gmail.com>
2023-02-12 18:43:43 +0300

    Добавьте файлы проекта.

---
+++ Laba7/Customer.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Variant16
+{
+    public class Customer
+    {
+        public string familyname;
+        public string name;
+        public string surname;
+        public string address;
+        public long phonenumber;
+        public long cardnumber;
+        public int bankaccountnumber { get; set; }
+
+        public string GetName
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string GetSurname
+        {
+            get
+            {
+                return surname;
+            }
+        }
+
+        public Customer(int i)
+        {
+            familyname = "unknown";
+            name = "unknown";
+            surname = "unknown";
+            address = "unknown";
+            phonenumber = 0;
+            cardnumber = 0;
+            bankaccountnumber = 0;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Customer(string familyname, string name, string surname, string address, long phonenumber, long cardnumber, int bankaccountnumber)
+        {
+            this.familyname = familyname;
+            this.name = name;
+            this.surname = surname;
+            this.address = address;
+            this.phonenumber = phonenumber;
+            this.cardnumber = cardnumber;
+            this.bankaccountnumber = bankaccountnumber;
+        }
+
+        public override string ToString()
+        {
+            return familyname + ", " + name + ", " + surname + ", " + address + ", " + phonenumber + ", " + cardnumber + ", " + bankaccountnumber;
+        }
+    }
+}
---
+++ Laba7/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Variant16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> customers = new List<Customer>
+{
+new Customer ("Лисичников", "Алексей", "Алексеевич", "Шуист", 89876543211, 7876542300008756, 0001),
+new Customer ("Антипкин", "Денис", "Павлович", "Телегино", 89875102996, 0040320878560000, 0002),
+new Customer ("Тупицин", "Илья", "Алексеевич", "Кургановка", 89560234501, 6700880823043498, 0003),
+new Customer ("Панькин", "Данила", "Сергеевич", "Kamenka", 89876590123, 4500300020207856, 0004)
+};
+
+            var sorted = customers.OrderBy(ob => ob.familyname).ToArray();
+            Console.WriteLine("Привет дружище, вот тебе список покупателей в алфавитном порядке: ");
+            Console.WriteLine("------------------------------------------------");
+            Array.ForEach(sorted, Console.WriteLine);
+            Console.WriteLine("------------------------------------------------");
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("А вот тебе список покупателей, номер кредитной карточки которых находится в заданном интервале: ");
+            Console.WriteLine("------------------------------------------------");
+            bool ex = false;
+            foreach (Customer c in customers)
+                if (c.cardnumber > 0098003456001200 && c.cardnumber < 8890440022004400)
+                {
+                    Console.WriteLine(c.ToString());
+                    ex = true;
+                }
+            Console.WriteLine("------------------------------------------------");
+
+        }
+    }
+}
